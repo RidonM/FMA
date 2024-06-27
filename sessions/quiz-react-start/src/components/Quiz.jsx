56301090efa14bd0c1cb38adb233db0c2544e331
RidonM <ridonmaliqi5@gmail.com>
2024-06-27 18:29:24 +0200

    27-06

--- sessions/quiz-react-start/src/components/Quiz.jsx
+++ sessions/quiz-react-start/src/components/Quiz.jsx
@@ -1,51 +1,57 @@
-import { useState } from 'react';
-import QUESTIONS from "../assets/js/questions.js"
-import quizCompleteScreen from '../assets/quiz-complete.png';
-import QuizTimer from './QuizTimer.jsx';
+import { useState, useCallback } from 'react';
+import QUESTIONS from '../assets/js/questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from './QuizTimer.jsx';
 
 export default function Quiz() {
-
     const [userAnswers, setUserAnswers] = useState([]);
+    const [points, setPoints] = useState(0);
     const activeQuestionIndex = userAnswers.length;
-    const isQuizComplete = activeQuestionIndex === QUESTIONS.length;
 
+    const quizIsCompelte = activeQuestionIndex === QUESTIONS.length;
 
-    function handleAnswer(selectedAnswer) {
-        setUserAnswers(
-            (previousAnswer) => {
-                return [...previousAnswer, selectedAnswer];
-            }
-        );
-    }
+    const handleSelectAnswers = useCallback(selectedAnswer => {
+        setUserAnswers(prevAnswers => [...prevAnswers, selectedAnswer]);
 
-    if (isQuizComplete) {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+            setPoints(prevPoints => prevPoints + 1);
+        }
+
+    }, [activeQuestionIndex]);
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswers(null), [handleSelectAnswers])
+
+    if (quizIsCompelte) {
         return (
             <div id="summary">
-                <img src={quizCompleteScreen} alt="Quiz Completed" />
+                <img src={quizCompleteImg} alt="Quiz Completed" />
                 <h2>Quiz Complete</h2>
+                <h2 className='text-center'>{points} / {QUESTIONS.length}</h2>
             </div>
+
         );
     }
 
-    const shuffleAnswers = [...QUESTIONS[activeQuestionIndex].answers.sort((a, b) => 0.5 - Math.random())]
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     return (
-        <main>
-            <div id="quiz">
-                <div id="question">
-                    <QuizTimer />
-                    <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                    <ul id="answers">
-                        {
-                            shuffleAnswers.map((answer) =>
-                                <li key={answer} className="answer">
-                                    <button onClick={() => handleAnswer(answer)}>{answer}</button>
-                                </li>
-                            )
-                        }
-                    </ul>
-                </div>
+        <div id="quiz">
+            <div id="question">
+                <QuestionTimer
+                    key={activeQuestionIndex}
+                    timeout={10000} //10 seconds in miliseconds
+                    onTimeout={handleSkipAnswer}
+                />
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {shuffledAnswers.map((answer) =>
+                        <li key={answer} className='answer'>
+                            <button onClick={() => handleSelectAnswers(answer)}>{answer}</button>
+                        </li>
+                    )}
+                </ul>
             </div>
-        </main>
+        </div>
     );
 }
--- sessions/quiz-react-start/src/components/QuizTimer.jsx
+++ sessions/quiz-react-start/src/components/QuizTimer.jsx
@@ -1,31 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-export default function QuizTimer() {
-    const [timer, setTimer] = useState(100);
+export default function QuestionTimer({ timeout, onTimeout }) {
+    const [remainingTime, setRemainingTime] = useState(timeout);
 
+    // Execute onTimeout after timeout duration
     useEffect(() => {
-        const timeInt = setInterval(() => {
-            setTimer(prevTimer => {
-                if (prevTimer > 0) {
-                    return prevTimer - 1;
-                } else {
-                    clearInterval(timeInt);
-                    return 0;
+        const timer = setTimeout(onTimeout, timeout);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [onTimeout, timeout]);
+
+    // Update remainingTime every 100ms
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRemainingTime(prevRemainingTime => {
+                const newRemainingTime = prevRemainingTime - 100;
+                if (newRemainingTime <= 0) {
+                    clearInterval(interval);
+                    return 0;  // Ensure it doesn't go negative
                 }
+                return newRemainingTime;
             });
         }, 100);
 
-
-        return () => clearInterval(timeInt);
+        return () => {
+            clearInterval(interval);
+        };
     }, []);
 
     return (
-        <>
-            <progress id="question-time" max="100" value={timer} />
-        </>
+        <progress id="question-time" max={timeout} value={remainingTime} />
     );
 }
-
-
-
-
